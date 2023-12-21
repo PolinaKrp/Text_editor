b91a6a59dda98bae8f9adc4cb534db96058180a6
Polina <114875941+PolinaKrp@users.noreply.github.com>
2023-12-21 22:34:23 +0400

    Добавлена функция для скачивания файла на компьтер

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtPrintSupport import *
 from PyQt5.QtCore import *
-from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication
+from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication,QFileDialog,QHBoxLayout,QWidget,QApplication
 import sys
 
 
@@ -60,9 +60,25 @@ def SaveTextFile():
     else:
         return False
 
+def SaveAsTextFile(self):
+    fileName, _ = QFileDialog.getSaveFileName(win,"Сохранить файл","","HTML (*.html)")
+    if fileName:
+        document = text.document()
+        file = QFile(f"{fileName}.html")
+        if file.open(QFile.WriteOnly):
+            OutputText = document.toHtml()
+            file.write(OutputText.encode('utf-8'))
+            file.close()
+            return True
+        else:
+            return False
+
 def LoadTextFile():
-    f = QFile("file.html")
-    f.open(QFile.ReadOnly | QFile.Text)
-    istream = QTextStream(f)
-    text.setHtml(istream.readAll())
-    f.close()
+    fileName=QFileDialog.getOpenFileName(win,"Открыть файл","","HTML (*.html)")
+
+    if (fileName[0]):
+        f = QFile(fileName[0])
+        f.open(QFile.ReadOnly | QFile.Text)
+        istream = QTextStream(f)
+        text.setHtml(istream.readAll())
+        f.close()
